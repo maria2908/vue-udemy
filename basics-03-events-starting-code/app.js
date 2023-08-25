0c0407e4d58916443a5641b95b51321eb77712dc
maho1 <mariia.honchariuk@sobaco-betax.com>
2023-08-25 13:46:52 +0200

    first commit

---
+++ basics-03-events-starting-code/app.js
@@ -0,0 +1,29 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: '',
+      confirmedName: ''
+    };
+  },
+  methods: {
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm(event) {
+      alert('Submited')
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+    }
+
+  }
+});
+
+app.mount('#events');
